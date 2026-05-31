e73c16e0ab371a144dda7d989c37066ba7ec43bc
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2026-05-31 16:34:09 +0300

    SetupMessage: rename DefaultSetupMessage to SetupPayload

--- rsocket-messages/src/main/java/com/jauntsdn/rsocket/SetupMessage.java
+++ rsocket-messages/src/main/java/com/jauntsdn/rsocket/SetupMessage.java
@@ -19,6 +19,7 @@ package com.jauntsdn.rsocket;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.util.ReferenceCounted;
+import javax.annotation.Nullable;
 
 /** Channel setup {@link Message} with data & metadata MIME types. */
 public abstract class SetupMessage implements ReferenceCounted {
@@ -30,7 +31,8 @@ public abstract class SetupMessage implements ReferenceCounted {
 
   public abstract Message message();
 
-  static final class DefaultSetupMessage extends SetupMessage {
+  static final class SetupPayload extends SetupMessage {
+    private final ByteBuf resumeToken;
     private final boolean leaseEnabled;
     private final int keepAliveInterval;
     private final int keepAliveMaxlifetime;
@@ -39,7 +41,8 @@ public abstract class SetupMessage implements ReferenceCounted {
     private final Message message;
     private final int version;
 
-    private DefaultSetupMessage(
+    private SetupPayload(
+        ByteBuf resumeToken,
         boolean leaseEnabled,
         int version,
         int keepAliveInterval,
@@ -47,6 +50,7 @@ public abstract class SetupMessage implements ReferenceCounted {
         String metadataMimeType,
         String dataMimeType,
         Message message) {
+      this.resumeToken = resumeToken;
       this.leaseEnabled = leaseEnabled;
       this.version = version;
       this.keepAliveInterval = keepAliveInterval;
@@ -56,9 +60,9 @@ public abstract class SetupMessage implements ReferenceCounted {
       this.message = message;
     }
 
-    public static DefaultSetupMessage create(
+    public static SetupPayload create(
         boolean resumeEnabled,
-        ByteBuf resumeToken,
+        @Nullable ByteBuf resumeToken,
         boolean leaseEnabled,
         int version,
         int keepAliveTickPeriod,
@@ -66,7 +70,8 @@ public abstract class SetupMessage implements ReferenceCounted {
         String metadataMimeType,
         String dataMimeType,
         Message setupMessage) {
-      return new DefaultSetupMessage(
+      return new SetupPayload(
+          resumeToken(resumeEnabled, resumeToken),
           leaseEnabled,
           version,
           keepAliveTickPeriod,
@@ -108,11 +113,11 @@ public abstract class SetupMessage implements ReferenceCounted {
     }
 
     public boolean isResumeEnabled() {
-      return false;
+      return resumeToken.readableBytes() > 0;
     }
 
     public ByteBuf resumeToken() {
-      return Unpooled.EMPTY_BUFFER;
+      return resumeToken;
     }
 
     @Override
@@ -152,6 +157,13 @@ public abstract class SetupMessage implements ReferenceCounted {
     @Override
     public boolean release(int decrement) {
       return message.release(decrement);
+    }
+
+    private static ByteBuf resumeToken(boolean resumeEnabled, @Nullable ByteBuf resumeToken) {
+      if (resumeEnabled && resumeToken != null) {
+        return resumeToken;
+      }
+      return Unpooled.EMPTY_BUFFER;
     }
   }
 }
